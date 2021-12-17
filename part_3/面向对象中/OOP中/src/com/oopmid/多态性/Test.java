faf7d9fd35eef6df2f2a30bf8126d2322b51f6dd
wong <l>
2021-12-17 16:12:34 +0800

    Changed

--- part_3/面向对象中/OOP中/src/com/oopmid/多态性/Test.java
+++ part_3/面向对象中/OOP中/src/com/oopmid/多态性/Test.java
@@ -4,6 +4,28 @@ public class Test {
 	public static void main(String[] args) {
 		
 		Person p = new Person();
+		p.eat();
+		p.walk();
+		p.name = "张三";
+		p.age = 27;
+		System.out.println("name: "+p.name+" age: "+p.age);
+		System.out.println("**************************************");
+		Person m = new Man(); 
+		//m.earMoney();
+		m.eat();
+		m.walk();
+		m.name  = "唐伯虎";
+		m.age = 24;
+		System.out.println("name: "+m.name+" age: "+m.age);
+		System.out.println("**************************************");
+		
+		Person w = new Woman();
+		w.eat();
+		w.walk();
+		w.name = "秋香";
+		w.age = 18;
+		System.out.println("name: "+w.name+" age: "+w.age);
+		
 		
 	}
 }
