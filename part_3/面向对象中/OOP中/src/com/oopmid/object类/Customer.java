f0ba5a174c6b298478442d10429ff5ec4e62cc0a
wong <l>
2022-01-05 17:13:42 +0800

    自动生成equals方法

--- part_3/面向对象中/OOP中/src/com/oopmid/object类/Customer.java
+++ part_3/面向对象中/OOP中/src/com/oopmid/object类/Customer.java
@@ -30,25 +30,48 @@ public class Customer {
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
-	// 重写Object中的equals()方法
+
+	//(shift+alt+s)自动生成 equals()
 	@Override
 	public boolean equals(Object obj) {
-		if(this == obj) {
+		if (this == obj)
 			return true;
-		}
-		if(obj instanceof Customer) {
-			Customer cust = (Customer)obj;
-			//比较两个对象的每个属性都相同
-//			if(this.age == cust.age && this.name.equals(cust.name)) {
-//				return true;
-//			}else {
-//				return false;
-//			}
-			return this.age == cust.age && this.name.equals(cust.name);
-//			// age是int类型基本数据类型用==比较，name是string引用类型，只能使用String类重写的equals()方法
-		}
-		return false;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (age != other.age)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
+	
+
+	
+	//自己手写
+	// 重写Object中的equals()方法
+//	@Override
+//	public boolean equals(Object obj) {
+//		if(this == obj) {
+//			return true;
+//		}
+//		if(obj instanceof Customer) {
+//			Customer cust = (Customer)obj;
+//			//比较两个对象的每个属性都相同
+////			if(this.age == cust.age && this.name.equals(cust.name)) {
+////				return true;
+////			}else {
+////				return false;
+////			}
+//			return this.age == cust.age && this.name.equals(cust.name);
+////			// age是int类型基本数据类型用==比较，name是string引用类型，只能使用String类重写的equals()方法
+//		}
+//		return false;
+	//}
 	
 }
