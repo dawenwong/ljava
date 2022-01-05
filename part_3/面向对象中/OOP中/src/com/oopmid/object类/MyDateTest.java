d5d84675773a5378f9d1e4d12a4f6e21bf0fe298
wongdawen <15922753630@163.com>
2022-01-05 22:44:33 +0800

    exercise2

---
+++ part_3/面向对象中/OOP中/src/com/oopmid/object类/MyDateTest.java
@@ -0,0 +1,96 @@
+package com.oopmid.object类;
+
+public class MyDateTest {
+	public static void main(String[] args) {
+		MyDate m1 = new MyDate(14, 3, 1976);
+		MyDate m2 = new MyDate(14, 3, 1976);
+		if(m1 == m2) {
+			System.out.println("m1==m2");
+		}else {
+			System.out.println("m1!=m2");
+		}
+		
+		if(m1.equals(m2)) {
+			System.out.println("m1 is equal to m2");
+		}else {
+			System.out.println("m1 is not equal to m2");
+		}
+	}
+}
+
+class MyDate{
+	private int day;
+	private int month;
+	private int year;
+	public MyDate(int day,int month,int year) {
+		super();
+		this.day = day;
+		this.month = month;
+		this.year = year;
+	}
+	
+	public void setDay(int day) {
+		this.day = day;
+	}
+	public int getDay() {
+		return day;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+
+	//自动生成的
+//	@Override
+//	public boolean equals(Object obj) {
+//		if (this == obj)
+//			return true;
+//		if (obj == null)
+//			return false;
+//		if (getClass() != obj.getClass())
+//			return false;
+//		MyDate other = (MyDate) obj;
+//		return day == other.day && month == other.month && year == other.year;
+//	}
+	
+	
+	//自己手写equals()方法
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj) {
+			return true;
+		}
+		if(obj instanceof MyDate) {
+			MyDate myDate = (MyDate)obj;
+			return this.day == myDate.day && this.month == myDate.month && this.year == myDate.year;
+		}
+		return false;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
