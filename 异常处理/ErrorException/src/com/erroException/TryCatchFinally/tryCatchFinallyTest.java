b9d004657ef18410621dd140ba12264e120b5702
wongdawen <15922753630@163.com>
2022-01-11 00:04:21 +0800

    异常处理

---
+++ 异常处理/ErrorException/src/com/erroException/TryCatchFinally/tryCatchFinallyTest.java
@@ -0,0 +1,24 @@
+package com.erroException.TryCatchFinally;
+/*
+ * 
+ * 异常的处理：抓抛模型
+ * 
+ * 过程一：“抛”：程序在正常执行的过程中，一旦出现异常，就会在异常代码处生产一个对应异常类的对象
+ * 				并将此对象抛出。
+ * 				一旦抛出对象后，其后的代码就不在执行
+ * 
+ * 
+ * 过程二：“抓”：可以理解异常的处理方式：try-catch-finally 和 throws 
+ * 			
+ * 
+ * 
+ * 
+ * java异常处理方式：
+ * 	方式一：try-catch-finally
+ * 
+ * 	方式二：throws + 异常处理
+ * 
+ * */
+public class tryCatchFinallyTest {
+	
+}
