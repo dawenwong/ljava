f525c2890f19bce8d5360ddfc5401cf35c1d0345
wongdawen <15922753630@163.com>
2022-01-13 22:07:01 +0800

    线程优先级

--- JavaSenior/Thread/src/com/hpc/thread/ThreadMethod.java
+++ JavaSenior/Thread/src/com/hpc/thread/ThreadMethod.java
@@ -15,6 +15,18 @@ package com.hpc.thread;
  *                          当前线程是阻塞状态。
  *  10.isAlive():判断当前线程是否存活
  *
+ *  线程的优先级：
+ *  1.
+ *  MAX_PRIORITY:10
+ *  MIN_PRIORITY:1
+ *  NORM_PRIORITY:5  -->默认优先级
+ *  2.如何获取和设置当前线程的优先级：
+ *      getPriority():获取线程的优先级
+ *      setPriority(int p):设置线程的优先级
+ *
+ *      说明：高优先级的线程要抢占低优先级线程cpu的执行权，但是只是从概率上讲，高优先级的线程高概率的情况下
+ *      被执行。并不意味只有当高优先级线程执行完后，低优先级线程才执行。
+ *
  *
  *  alt+7 打开结构图，查看方法、构造器
  *  ctrl+ Fn + F12 打开弹窗的形式查看当前类的方法、构造器
@@ -28,11 +40,12 @@ class HelloThread extends Thread{
             if(i % 2 == 0){
                 try {
 //                    Thread.sleep(1000);  //单位是ms(毫秒)
-                    sleep(100); //省略Thread
+                    sleep(10); //省略Thread
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println(Thread.currentThread().getName()+" : "+i);
+                System.out.println(Thread.currentThread().getName()+" : "+
+                        Thread.currentThread().getPriority()+" : "+i);
             }
 //            if(i % 20 == 0){
 //                this.yield();
@@ -48,13 +61,19 @@ public class ThreadMethod {
     public static void main(String[] args) {
         HelloThread helloThread = new HelloThread("Thread:1");
 //        helloThread.setName("线程一"); // 命名方式一
+        //设置分线程的优先级
+        helloThread.setPriority(Thread.MAX_PRIORITY); // 设置为MAX_PRORITY,(10)
+
         helloThread.start();
 
         // 给主线程命名
         Thread.currentThread().setName("主线程");
+        // 设置主线程的优先级
+        Thread.currentThread().setPriority(Thread.MIN_PRIORITY); //给主线程设置为最小
         for (int i = 0; i < 100; i++) {
             if(i % 2 == 0){
-                System.out.println(Thread.currentThread().getName()+" : "+i);
+                System.out.println(Thread.currentThread().getName()+" : "+
+                        Thread.currentThread().getPriority()+" : "+i);
             }
             //join()方法
             if(i == 20){
