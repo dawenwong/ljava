a96e975a1ca0b6641468ffa21f63a0e8d70d619f
wongdawen <15922753630@163.com>
2022-01-13 21:49:10 +0800

    方法使用

--- JavaSenior/Thread/src/com/hpc/thread/ThreadMethod.java
+++ JavaSenior/Thread/src/com/hpc/thread/ThreadMethod.java
@@ -8,7 +8,13 @@ package com.hpc.thread;
  *  4.getName():获取当前线程的名字
  *  5.setName():设置当前线程的名字
  *  6.yield():释放当前cpu的执行权
- *  7.join():
+ *  7.join():在线程a中调用线程b的join()，此时线程a就进入阻塞状态，直到线程b完全执行结束，
+ *          线程a才结束阻塞状态。
+ *  8.stop():已经过时。当前执行此方法时，强制结束当前线程。
+ *  9.sleep(long millitime):让当前线程“睡眠”指定的millistime毫秒。在指定的时间内，
+ *                          当前线程是阻塞状态。
+ *  10.isAlive():判断当前线程是否存活
+ *
  *
  *  alt+7 打开结构图，查看方法、构造器
  *  ctrl+ Fn + F12 打开弹窗的形式查看当前类的方法、构造器
@@ -17,9 +23,15 @@ package com.hpc.thread;
  * */
 class HelloThread extends Thread{
     @Override
-    public void run() {
+    public void run() { // Thread 没有throws，所以子类重写也不能throws
         for (int i = 0; i < 100; i++) {
             if(i % 2 == 0){
+                try {
+//                    Thread.sleep(1000);  //单位是ms(毫秒)
+                    sleep(100); //省略Thread
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 System.out.println(Thread.currentThread().getName()+" : "+i);
             }
 //            if(i % 20 == 0){
@@ -54,5 +66,6 @@ public class ThreadMethod {
             }
 
         }
+        System.out.println(helloThread.isAlive()); // 不存活：返回false
     }
 }
