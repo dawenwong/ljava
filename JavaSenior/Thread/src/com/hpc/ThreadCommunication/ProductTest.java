82ccddf6ee1bf2490e15448b894e75739ba05a1b
wong <l>
2022-01-18 09:54:27 +0800

    生产者消费者例子

--- JavaSenior/Thread/src/com/hpc/ThreadCommunication/ProductTest.java
+++ JavaSenior/Thread/src/com/hpc/ThreadCommunication/ProductTest.java
@@ -11,6 +11,94 @@ package com.hpc.ThreadCommunication;
  *
  *
  */
-public class ProductTest {
 
+class Clerk{
+    private int productCount = 0;
+
+    //生产产品
+    public synchronized void produceProduct() {
+        if(productCount < 20){
+            productCount++;
+            System.out.println(Thread.currentThread().getName()+"开始生产第" + productCount + "个产品了");
+            notify();
+        }else{
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //消费产品
+    public synchronized void consumeProduct() {
+        if(productCount > 0){
+            System.out.println(Thread.currentThread().getName() + "开始消费第" + productCount + "个产品了");
+            productCount--;
+
+            notify();
+        }else{
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+class Producer extends Thread{ //生产者
+    private Clerk clerk;
+
+    public Producer(Clerk clerk) {
+        this.clerk = clerk;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + ":开始生产产品");
+        while(true){
+
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            clerk.produceProduct();
+        }
+    }
+}
+class Customer extends Thread{ //消费者
+    private Clerk clerk;
+
+    public Customer(Clerk clerk) {
+        this.clerk = clerk;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(getName() + ":消费者开始消费了");
+        while(true){
+            try{
+                Thread.sleep(100);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            clerk.consumeProduct();
+        }
+    }
+}
+
+public class ProductTest {
+    public static void main(String[] args) {
+        Clerk clerk = new Clerk();
+        Producer p1 = new Producer(clerk);
+        p1.setName("生产者一");
+        Customer c1 = new Customer(clerk);
+        c1.setName("消费者一");
+
+        p1.start();
+        c1.start();
+
+    }
 }
